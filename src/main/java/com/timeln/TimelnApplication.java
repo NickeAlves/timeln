0f6fde811a4e78fa1140c537dcdc09a8737efef1
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-03-21 18:42:59 +0100

    :sparkles: init API project
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

---
+++ src/main/java/com/timeln/TimelnApplication.java
@@ -0,0 +1,13 @@
+package com.timeln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeln/TimelnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
